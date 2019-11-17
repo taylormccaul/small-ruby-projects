f0b922dae91904b93a6ef7e0e685b189f595193e
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2019-11-16 20:39:02 -0500

    Add input loop

--- shapes_ascii.rb
+++ shapes_ascii.rb
@@ -46,11 +46,21 @@ def square
   puts "What size do you want your square to be?"
   square_size = gets.chomp.to_i
 
-  square_char = "#"
+  puts "What character do you want to use to make the square?"
+  square_type = gets.chomp
+  if square_type == ""
+    square_type = "#"
+  end
+
+  while square_type.length.to_i > 1
+    puts "Please enter a single character."
+    square_type = gets.chomp
+  end
+
   length = 1
 
   (square_size / 2).times do
-    puts square_char * square_size
+    puts square_type * square_size
   end
 end
 
@@ -58,16 +68,26 @@ def diamond
   puts "What size diamond do you want?"
   diamond_size = gets.chomp.to_i
 
-  diamond_char = "#"
+  puts "What character do you want to use to make the diamond?"
+    diamond_type = gets.chomp
+  if diamond_type == ""
+    diamond_type = "#"
+  end
+
+  while diamond_type.length.to_i > 1
+    puts "Please enter a single character."
+    diamond_type = gets.chomp
+  end
+
   start_width = 1
 
   while start_width <= diamond_size
-    puts (diamond_char * start_width).center(100)
+    puts (diamond_type * start_width).center(100)
     start_width += 2
   end
 
   while start_width >= 1
-    puts (diamond_char * start_width).center(100)
+    puts (diamond_type * start_width).center(100)
     start_width -= 2
   end
 end
@@ -77,52 +97,44 @@ def rectangle
   rec_length = gets.chomp.to_i
   puts "How wide do you want the rectangle to be?"
   rec_width = gets.chomp.to_i
+  puts "What character do you want to use to make the rectangle?"
+    rec_type = gets.chomp
+  if rec_type == ""
+    rec_type = "#"
+  end
 
-  rec_char = "#"
+  while rec_type.length.to_i > 1
+    puts "Please enter a single character."
+    rec_type = gets.chomp
+  end
+
   size = 1
 
   while size <= rec_length.to_i
-    puts (rec_char * rec_width).center(100)
+    puts (rec_type * rec_width).center(100)
     size += 1
   end
 end
 
-#def circle
-#  puts "What radius circle do you want?"
-#  circle_radius = gets.chomp.to_i
-
-#  circle_char = "#"
-#  width = 3
-
-#  while width <= (circle_radius + 1)
-#    puts (circle_char * width).center(100)
-#    width += 2
-#  end
-
-#  3.times do
-#    puts (circle_char * width).center(100)
-#  end
-
-#  while width >= 3
-#    puts (circle_char * width).center(100)
-#    width -= 2
-#  end
-##  if width == circle_radius + 1
-##    while width >= 3
-##      puts circle_char * width
-##      width -= 2
-##    end
-##  end
-#end
-
-puts "What shape would you like to make?\nOptions:\npyramid\nsquare\ndiamond\nrectangle"
-user_input = gets.chomp
-if user_input.downcase == "pyramid"
-  puts pyramid
-elsif user_input.downcase == "square"
-  puts square
-elsif user_input.downcase == "diamond"
-  puts diamond
-elsif user_input.downcase == "rectangle"
-  puts rectangle
+user_input = ""
+while user_input.downcase != "no"
+  puts "What shape would you like to make?\nOptions:\nPyramid\nSquare\nDiamond\nRectangle"
+    user_input = gets.chomp
+  if user_input.downcase == "pyramid"
+    puts pyramid
+    puts "Would you like to make another shape?"
+    user_input = gets.chomp
+  elsif user_input.downcase == "square"
+    puts square
+    puts "Would you like to make another shape?"
+    user_input = gets.chomp
+  elsif user_input.downcase == "diamond"
+    puts diamond
+    puts "Would you like to make another shape?"
+    user_input = gets.chomp
+  elsif user_input.downcase == "rectangle"
+    puts rectangle
+    puts "Would you like to make another shape?"
+    user_input = gets.chomp
+  end
 end
