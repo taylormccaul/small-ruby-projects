8a4587c99a02abca5eef36b6bcae74570820d04b
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2019-11-18 23:03:33 -0500

    Package code in method

--- file_size_sorter.rb
+++ file_size_sorter.rb
@@ -9,20 +9,25 @@
 #end
 #file_sizes = []
 #file_names = []
-puts "Specify a file path. If left blank, the current directory will be chosen by default."
-file_dir = gets.chomp
-if file_dir == "" || file_dir == " "
-	file_dir = "./"
-end
-puts "Enter which file extension you would like to sort by. Ex: .txt, .png, .pdf. If left blank, all files in the directory will be listed."
-file_ext_name = gets.chomp
 
-Dir.foreach(file_dir) do |file|
-	if file_ext_name == "" || file_ext_name == " "
-		next if file == "." || file == ".." || File::directory?(file) || /^\./ =~ file || /^[ntNT]/ =~ file
-		puts file
-	else
-		next if File.extname(file) != file_ext_name
-		puts file
+def find_ext_name
+	puts "Specify a file path. If left blank, the current directory will be chosen by default."
+	file_dir = gets.chomp
+	if file_dir == "" || file_dir == " "
+		file_dir = "./"
+	end
+	puts "Enter which file extension you would like to sort by. Ex: .txt, .png, .pdf. If left blank, all files in the directory will be listed."
+	file_ext_name = gets.chomp
+
+	Dir.foreach(file_dir) do |file|
+		if file_ext_name == "" || file_ext_name == " "
+			next if file == "." || file == ".." || File::directory?(file) || /^\./ =~ file || /^[ntNT]/ =~ file
+			puts file
+		else
+			next if File.extname(file) != file_ext_name
+			puts file
+		end
 	end
 end
+
+puts find_ext_name
