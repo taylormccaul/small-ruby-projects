7cdccafeac92f8c86378250b150926e1fe993af6
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2019-11-18 23:01:42 -0500

    Add ext method

--- file_size_sorter.rb
+++ file_size_sorter.rb
@@ -9,22 +9,20 @@
 #end
 #file_sizes = []
 #file_names = []
-puts "Specify a file path: "
+puts "Specify a file path. If left blank, the current directory will be chosen by default."
 file_dir = gets.chomp
+if file_dir == "" || file_dir == " "
+	file_dir = "./"
+end
+puts "Enter which file extension you would like to sort by. Ex: .txt, .png, .pdf. If left blank, all files in the directory will be listed."
+file_ext_name = gets.chomp
 
 Dir.foreach(file_dir) do |file|
-	next if file == "." || file == ".." || File::directory?(file) || /^\./ =~ file || /^[ntNT]/ =~ file
-	#file_size = file.size
-	#file_sizes.push(file_size)
-	#file_names.push(file)
-	#file_sizes = file_sizes.sort.reverse
-	#next if File::directory?(file)
-	#next if /^\./ =~ file || /^[ntNT]/ =~ file
-	print file + " - "
-	puts (file.size).to_s + "B"
+	if file_ext_name == "" || file_ext_name == " "
+		next if file == "." || file == ".." || File::directory?(file) || /^\./ =~ file || /^[ntNT]/ =~ file
+		puts file
+	else
+		next if File.extname(file) != file_ext_name
+		puts file
+	end
 end
-
-#Dir.foreach(file_dir) do |file|
-#	next if file.extname != file_dir
-#	puts file
-#end
