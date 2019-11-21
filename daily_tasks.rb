5b6113a33c89adeddc3ce10d7946c02ab6eefc76
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2019-11-20 22:53:26 -0500

    Add tasks method

--- daily_tasks.rb
+++ daily_tasks.rb
@@ -1,1 +1,17 @@
+def tasks
+    tasks = ""
+    puts "Enter a date: "
+    date_time = gets.chomp
+    puts "Type a task to add: "
+    tasks_to_add = gets.chomp
+    tasks_file.puts("#{date_time}:\n")
+    while tasks_to_add.downcase != "end"
+        #tasks += "#{tasks_to_add}\n"
+	      tasks_file.puts("#{tasks_to_add}\n")
+	      tasks_to_add = gets.chomp	    
+    end
+    tasks_file.puts("\n")
+    #puts tasks
+end
 
+puts tasks
