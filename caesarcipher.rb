713397f6e1124ce86fb73adc3d44b0c4c20b1f19
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2019-11-22 17:10:23 -0500

    First commit

---
+++ caesarcipher.rb
@@ -0,0 +1,19 @@
+#puts "Enter a word or phrase to encrypt."
+#user_word = gets.chomp
+puts "Enter the amount of letters you want to shift over."
+shift = gets.chomp.to_i
+letters_arr = "abcdefghijklmnopqrstuvwxyz".split("")
+counter = 0
+letters_arr.each do
+    letters_arr[counter] = (letters_arr[counter].ord + shift).chr
+    #puts "#{letters_arr[counter]}: " + letters_arr[counter].ord.to_s
+
+    if letters_arr[counter].ord.to_i > 122
+        puts "#{(letters_arr[counter].ord - 25).chr}: " + (letters_arr[counter].ord - 25).to_s
+    else
+	puts "#{letters_arr[counter]}: " + (letters_arr[counter].ord.to_s)
+    end
+
+    counter += 1
+end
+#puts "a".ord
