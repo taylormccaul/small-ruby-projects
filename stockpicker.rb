f7fa05e4fccb21cc0b687b9ba8f07aaf60cd91a1
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2019-11-30 23:06:44 -0500

    Add pseudocode

---
+++ stockpicker.rb
@@ -0,0 +1,12 @@
+loop over elements of array once
+store the first number in a variable (greatestNum)
+if the array element i is greater in value than the variable, use that value for the variable
+store the index of greatestNum in a variable (use index method)
+
+loop over elements of array once more
+store the first number in a variable (smallestNum)
+if the array element i is greater in value than the variable, use that value for the variable
+store the index of smallestNum in a variable
+
+push the indexes of greatestNum and smallestNum into an array
+return the array
